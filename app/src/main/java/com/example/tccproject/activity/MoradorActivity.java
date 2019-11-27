023b3da8c5117b24c682eb832a1bb66b07baf16a
Joao <joaomarco18@hotmail.com>
2019-11-27 12:48:21 -0300

    commit de funcionalidades do firebase

--- app/src/main/java/com/example/tccproject/activity/MoradorActivity.java
+++ app/src/main/java/com/example/tccproject/activity/MoradorActivity.java
@@ -1,19 +1,33 @@
 package com.example.tccproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MoradorActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
+    AppBarConfiguration appBarConfiguration;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
     protected void onStart() {
         super.onStart();
@@ -25,6 +39,14 @@ public class MoradorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morador);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment).getHost();
+
+
+
 
         Button signOutButton = findViewById(R.id.signOutButton);
         mAuth = FirebaseAuth.getInstance();
@@ -46,4 +68,13 @@ public class MoradorActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
 }
